9e04a86b85ba8dbf5f70771e4c989801fbd2a0d2
muhannad <mohndalzaher@gmail.com>
2024-07-31 11:40:56 +0300

    new update

--- bin/firstapp.dart
+++ bin/firstapp.dart
@@ -2,8 +2,13 @@ import 'package:firstapp/firstapp.dart' as firstapp;
 
 void main(List<String> arguments) {
   muhannad(12, 3);
+  max("muhannd", "singal");
 }
 
 muhannad(int a, int b) {
   return a + b;
+}
+
+max(String name, String status) {
+  print("your name is $name and you are $status");
 }
