93a81c871d91272e3923700ed59fe963e41a0ad1
muhannad <mohndalzaher@gmail.com>
2024-07-31 11:50:20 +0300

    peca

--- bin/firstapp.dart
+++ bin/firstapp.dart
@@ -3,6 +3,7 @@ import 'package:firstapp/firstapp.dart' as firstapp;
 void main(List<String> arguments) {
   muhannad(12, 3);
   max("muhannd", "singal");
+  pica("aberr");
 }
 
 muhannad(int a, int b) {
@@ -11,4 +12,8 @@ muhannad(int a, int b) {
 
 max(String name, String status) {
   print("your name is $name and you are $status");
+}
+
+pica(String buyite) {
+  print("U are buty: $buyite");
 }
