a149686f7948b5020ed23887bec6412e3424a795
muhannad <mohndalzaher@gmail.com>
2024-08-01 13:33:01 +0300

    Create project OOP

---
+++ bin/oopemp.dart
@@ -0,0 +1,5 @@
+import 'package:oopemp/oopemp.dart' as oopemp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oopemp.calculate()}!');
+}
---
+++ test/oopemp_test.dart
@@ -0,0 +1,8 @@
+import 'package:oopemp/oopemp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
