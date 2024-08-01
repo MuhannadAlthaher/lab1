bf71e818368b099ae2d7760e952a0dd52c8e3513
muhannad <mohndalzaher@gmail.com>
2024-08-01 13:54:23 +0300

    class employye created

---
+++ bin/employee.dart
@@ -0,0 +1,8 @@
+class Employee {
+  String? name;
+  int? ID;
+  String? birh_Day;
+  double? salary;
+
+Employee({required this.ID , required this.name , required this.birh_Day, required this.salary})
+}
